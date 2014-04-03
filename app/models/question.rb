5394b34e22e26e7b09207052c41d4d9255d76303
shanmuk <hb.shanmuk@hiddenbrains.in>
2014-04-03 21:27:47 +0530

    first commit

---
+++ app/models/question.rb
@@ -0,0 +1,12 @@
+class Question < ActiveRecord::Base
+	
+	PERSONALITY = {
+		1=>"Realistic",
+		2=>"Investigative",
+		3=>"Artistic",
+		4=>"Social",
+		5=>"Enterprising",
+		6=>"Conventional"
+	}
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonSurvey::Application.config.secret_key_base = '2292d3ca2424b0414da7c8e75004f1534f961e1b9a91749d341474cc23238a438cf6ec1e3543f9024c492bb0b9f70c0c3e0fcff1ecc5fadf0f1dc22a0f752c0f'
